031a79cdc0ef74a3567cb9808843c7a3135eb1d4
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-05-23 23:13:51 +0300

    Create 07_projects_creation.py

---
+++ Python Basics/Lab/L1/07_projects_creation.py
@@ -0,0 +1,6 @@
+name_of_architect = input()
+number_of_projects_to_complete = int(input())
+total_time_needed_to_complete_all_project = 3 * number_of_projects_to_complete
+
+print(f"The architect {name_of_architect} will need {total_time_needed_to_complete_all_project} hours to complete "
+      f"{number_of_projects_to_complete} project/s.")
