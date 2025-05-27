99ece7014ad910d35b19679d520f4548773e08d9
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-05-27 03:23:07 +0300

    Create 01_day_of_week.py

---
+++ Python Basics/Lab/L3/01_day_of_week.py
@@ -0,0 +1,27 @@
+day_number = int(input())
+reply = ""
+if day_number == 1:
+    reply = "Monday"
+
+elif day_number == 2:
+    reply = "Tuesday"
+
+elif day_number == 3:
+    reply = "Wednesday"
+
+elif day_number == 4:
+    reply = "Thursday"
+
+elif day_number == 5:
+    reply = "Friday"
+
+elif day_number == 6:
+    reply = "Saturday"
+
+elif day_number == 7:
+    reply = "Sunday"
+
+else:
+    reply = "Error"
+
+print(reply)
