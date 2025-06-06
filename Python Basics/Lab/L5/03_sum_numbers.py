17ae36c615a5db3600f986fdda9cbe2d25f3591d
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-06-06 04:12:22 +0300

    Create 03_sum_numbers.py

---
+++ Python Basics/Lab/L5/03_sum_numbers.py
@@ -0,0 +1,11 @@
+initial_number = int(input())
+
+sum_numbers = 0
+
+while sum_numbers < initial_number:
+    add_number = int(input())
+    sum_numbers += add_number
+else:
+    print(sum_numbers)
+
+
