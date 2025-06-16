5649364cab8c6eb9ace860a181906ca294ce9f8c
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-06-17 02:28:52 +0300

    Create 04_vacation_books_list.py

---
+++ Python Basics/Exercise/E1/04_vacation_books_list.py
@@ -0,0 +1,7 @@
+page_numbers = int(input())
+page_numbers_per_hour = int(input())
+number_of_days = int(input())
+hours_per_day_to_read = int(page_numbers / page_numbers_per_hour / number_of_days)
+
+print(hours_per_day_to_read)
+
