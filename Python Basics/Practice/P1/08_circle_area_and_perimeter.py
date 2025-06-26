69929e0acdaae8d78c39f805535a99f6b1ad8170
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-06-26 15:10:03 +0300

    Create 08_circle_area_and_perimeter.py

---
+++ Python Basics/Practice/P1/08_circle_area_and_perimeter.py
@@ -0,0 +1,10 @@
+
+
+from math import pi
+r = float(input())
+
+calculated_area = float(pi * r * r)
+calculated_parameter = float(2 * pi * r)
+
+print(f'{calculated_area:.2f}')
+print(f'{calculated_parameter:.2f}')
