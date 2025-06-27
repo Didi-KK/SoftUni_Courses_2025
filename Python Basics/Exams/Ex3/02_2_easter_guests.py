9c7bd433664d83725a06c4ff1442b892be8b15ef
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-06-27 03:39:19 +0300

    Create 02_2_easter_guests.py

---
+++ Python Basics/Exams/Ex3/02_2_easter_guests.py
@@ -0,0 +1,19 @@
+from math import ceil
+guests = int(input())
+budget = float(input())
+
+brioche = 4.00
+egg = 0.45
+
+
+brioche_num = ceil(guests / 3)
+egg_num = guests * 2
+expenses = brioche_num * brioche + egg_num * egg
+diff = budget - expenses
+if expenses <= budget:
+    print(f'Lyubo bought {brioche_num} Easter bread and {egg_num} eggs.'
+          f'\nHe has {diff:.2f} lv. left.')
+else:
+    print(f'Lyubo doesn\'t have enough money.'
+          f'\n'
+          f'He needs {abs(diff):.2f} lv. more.')
