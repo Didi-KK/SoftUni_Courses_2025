17cf91318ef9d95fee50ed85bfe1d98c3f8cdcf5
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-07-28 02:52:09 +0300

    Create 09_sorting_hat.py

---
+++ Python Fundamentals/01-Basic-Syntax-Conditional-Statements-and-Loops-Exercise/09_sorting_hat.py
@@ -0,0 +1,23 @@
+command = input()
+
+while command != "Welcome!":
+
+    if command == "Voldemort":
+        print(f"You must not speak of that name!")
+        quit()
+
+    if len(command) < 5:
+        print(f"{command} goes to Gryffindor.")
+
+    elif len(command) == 5:
+        print(f"{command} goes to Slytherin.")
+
+    elif len(command) == 6:
+        print(f"{command} goes to Ravenclaw.")
+
+    elif len(command) > 6:
+        print(f"{command} goes to Hufflepuff.")
+
+    command = input()
+print(f"Welcome to Hogwarts.")
+
