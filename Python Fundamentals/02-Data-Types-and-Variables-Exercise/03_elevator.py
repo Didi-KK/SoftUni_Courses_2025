92b82083cedd725402556425cf12bdf7ee31a5b5
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-02 02:49:24 +0300

    Create 03_elevator.py

---
+++ Python Fundamentals/02-Data-Types-and-Variables-Exercise/03_elevator.py
@@ -0,0 +1,8 @@
+from math import ceil
+
+number_of_people = int(input())
+capacity_of_the_elevator = int(input())
+
+elevator_courses = ceil(number_of_people / capacity_of_the_elevator)
+
+print(elevator_courses)
