17b152ac26517c83c07258eead8a4dbc55e38d27
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-02 02:49:15 +0300

    Create 01_integer_operations.py

---
+++ Python Fundamentals/02-Data-Types-and-Variables-Exercise/01_integer_operations.py
@@ -0,0 +1,8 @@
+integer_one = int(input())
+integer_two = int(input())
+integer_three = int(input())
+integer_four = int(input())
+
+final_result = (integer_one + integer_two) // integer_three * integer_four
+
+print(final_result)
