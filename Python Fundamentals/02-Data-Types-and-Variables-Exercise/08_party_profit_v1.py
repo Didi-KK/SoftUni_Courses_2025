ecce416408d6d0b010c2508e0a5282b33080249c
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-02 02:49:53 +0300

    Create 08_party_profit_v1.py

---
+++ Python Fundamentals/02-Data-Types-and-Variables-Exercise/08_party_profit_v1.py
@@ -0,0 +1,31 @@
+from math import floor
+
+coins = 0
+group_size = int(input())
+days_of_adventure = int(input())
+
+for day in range(1, days_of_adventure + 1):
+
+    coins += 50
+    coins -= group_size * 2
+
+    if day % 10 == 0:
+        group_size -= 2
+        coins += 2 * 2
+
+    if day % 15 == 0:
+        group_size += 5
+        coins -= 5 * 2
+
+    if day % 3 == 0:
+        coins -= group_size * 3
+
+    if day % 5 == 0:
+        coins += group_size * 20
+        if day % 3 == 0:
+            coins -= group_size * 2
+
+companion_coins = floor (coins / group_size)
+# companion_coins = coins // group_size
+
+print(f"{group_size} companions received {companion_coins} coins each.")
