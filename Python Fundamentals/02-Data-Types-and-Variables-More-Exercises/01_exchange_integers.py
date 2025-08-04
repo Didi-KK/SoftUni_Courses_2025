fb99d35cbb0fde6b4c6800eca4df1c73c1298113
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-04 12:21:46 +0300

    Create 01_exchange_integers.py

---
+++ Python Fundamentals/02-Data-Types-and-Variables-More-Exercises/01_exchange_integers.py
@@ -0,0 +1,10 @@
+integer_a = int(input())
+integer_b = int(input())
+
+temp_a = integer_a
+
+swapped_a = integer_b
+swapped_b = temp_a
+
+print(f"Before:\na = {integer_a}\nb = {integer_b}\nAfter:\na = {swapped_a}\nb = {temp_a}")
+
