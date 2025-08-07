76e6c77905705fd061f172c67485d02a32430166
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-07 15:32:44 +0300

    Create 08_seize_the_fire_v1.py

---
+++ Python Fundamentals/03-Lists-Basics-Exercise/08_seize_the_fire_v1.py
@@ -0,0 +1,28 @@
+fire = input().split("#")
+water = int(input())
+
+valid_cells = []
+effort = 0
+total_fire = 0
+
+for index in range(len(fire)):
+
+    type_of_fire, value_of_cell = fire[index].split(" = ")
+    value_of_cell = int(value_of_cell)
+
+    if water >= value_of_cell and ((type_of_fire == 'High' and value_of_cell in range(81, 126))
+            or (type_of_fire == 'Medium' and value_of_cell in range(51, 81))
+            or (type_of_fire == 'Low' and value_of_cell in range(1, 51))):
+
+        water -= value_of_cell
+        effort += value_of_cell * 0.25
+        total_fire += value_of_cell
+        valid_cells.append(value_of_cell)
+    else:
+        continue
+
+print(f"Cells:")
+for cell in valid_cells:
+    print(f" - {cell}")
+print(f"Effort: {effort:.2f}")
+print(f"Total Fire: {total_fire}")
