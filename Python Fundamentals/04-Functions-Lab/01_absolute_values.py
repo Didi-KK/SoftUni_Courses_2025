e1e20b115a85dc19c8a5808a27e3de9f82344649
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-13 21:27:53 +0300

    Create 01_absolute_values.py

---
+++ Python Fundamentals/04-Functions-Lab/01_absolute_values.py
@@ -0,0 +1,9 @@
+number_list = input().split()
+
+absolute_values = [abs(float(num)) for num in number_list]
+
+# absolute_values = []
+# for num in number_list:
+#     absolute_values.append(abs(float(num)))
+
+print(absolute_values)
