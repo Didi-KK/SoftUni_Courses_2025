7519872b3c9c9668f89f73816afb192671be8abd
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-22 23:43:56 +0300

    Create 03_kates_way_out.py

---
+++ Python Fundamentals/05-Lists-Advanced-More Exercises/03_kates_way_out.py
@@ -0,0 +1,14 @@
+# rows = int(input())
+# maze = [list(input()) for lines in range(rows)]
+# an_exit = ' '
+# can_not_escape = False
+#
+# if (an_exit not in maze[0]
+#         and an_exit not in maze[-1]
+#         and an_exit not in maze[0][0:-1]
+#         and an_exit not in maze[-1][0:-1]):
+#     can_not_escape = True
+#
+#
+# if can_not_escape:
+#     print(f"Kate cannot get out")
