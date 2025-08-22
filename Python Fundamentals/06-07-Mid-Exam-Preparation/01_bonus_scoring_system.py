73f09b73f0ec76575ddff7d619192dd6181da8cd
Didi_KK <211705015+Didi-KK@users.noreply.github.com>
2025-08-22 23:45:14 +0300

    Create 01_bonus_scoring_system.py

---
+++ Python Fundamentals/06-07-Mid-Exam-Preparation/01_bonus_scoring_system.py
@@ -0,0 +1,17 @@
+from math import ceil
+
+students = int(input())
+lectures = int(input())
+additional_bonus = int(input())
+max_bonus = 0
+max_attendance = 0
+
+for student in range(students):
+    attendance = int(input())
+    total_bonus = attendance / lectures * (5 + additional_bonus)
+    if total_bonus > max_bonus:
+        max_bonus = total_bonus
+        max_attendance = attendance
+
+print(f"Max Bonus: {ceil(max_bonus)}.")
+print(f"The student has attended {max_attendance} lectures.")
